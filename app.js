99a89930b6587473da2f58beb36cc448e728c8f5
Akshat Kala <72704561+ak-sudo@users.noreply.github.com>
2025-04-29 19:34:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -108,7 +108,9 @@ app.use((req, res, next) => {
   next();
 })
 
-app.listen(8080, () => {
+port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("Server is running on port 8080");
 });
 
